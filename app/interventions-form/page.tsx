46569a01659e586380ead01279f1e228626ff477
Antony <antomutwiri2001@gmail.com>
2025-05-23 12:49:08 +0300

    minor changes:

--- app/interventions-form/page.tsx
+++ app/interventions-form/page.tsx
@@ -12,8 +12,8 @@ function Page() {
         <p className="font-medium text-lg text-gray-800 mb-4">
           The relevant stakeholders including the Authority, policy makers, the academia, members of the public, health professional associations, civil society organizations involved in matters of health, the Kenya Medical Supplies Authority and the county governments may propose interventions for inclusion in the benefits packages under regulation.
         </p>
-        <p className="font-medium text-md text-gray-800 mb-4">
-          The proposals will be received by the HBTAP secretariat through various channels including direct requests by the Panel to key stakeholders; scheduled stakeholder meetings or forums; or digital platforms including hbtap@uonbi.ac.ke or by filling the form below.
+        <p className="font-medium text-lg text-gray-800 mb-4">
+          The proposals will be received by the HBTAP secretariat through various channels including direct requests by the Panel to key stakeholders; scheduled stakeholder meetings or forums; or digital platforms including <span className='text-[#27aae1]'>hbtap@uonbi.ac.ke</span> or by filling the form below.
         </p>
       </section>
       <BenefitsForm />

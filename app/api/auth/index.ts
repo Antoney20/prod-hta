010904cb6a88794ed047195ef4409801026c966a
Antony <antomutwiri2001@gmail.com>
2025-10-26 08:37:27 +0300

    added the api/ and the  robots.txt file

--- app/api/auth/index.ts
+++ app/api/auth/index.ts
@@ -3,8 +3,7 @@ import { jwtDecode } from 'jwt-decode';
 import { useState, useEffect } from 'react';
 
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
-const API_URL = process.env.API_URL || API_BASE_URL;
-
+const API_URL = `${process.env.API_URL || API_BASE_URL}/api`;
 
 const api = axios.create({
   baseURL: API_URL,

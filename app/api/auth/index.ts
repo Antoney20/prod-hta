ded5f77c709079051c011dbf1212b425a20ddc18
Antony <antomutwiri2001@gmail.com>
2026-03-16 13:16:32 +0300

    feat: role and permissions change,, worked on the not found page

--- app/api/auth/index.ts
+++ app/api/auth/index.ts
@@ -2,13 +2,13 @@ import axios from 'axios';
 import { jwtDecode } from 'jwt-decode';
 import { useState, useEffect } from 'react';
 
-// const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
-// const API_URL = `${process.env.API_URL || API_BASE_URL}`;
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+const API_URL = `${process.env.API_URL || API_BASE_URL}`;
 
-const API_URL = '/api';  
+// const API_URL = '/api';  
 
 
-export type UserRole = 'admin' | 'secretariate' | 'content_manager' | 'user' | 'swg';
+export type UserRole = 'admin' | 'secretariat' | 'content_manager' | 'user' | 'swg';
 
 
 const api = axios.create({

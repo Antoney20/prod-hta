8aa95c749f829bc889fce9e34ce22d4d14a755b5
Antony <antomutwiri2001@gmail.com>
2026-08-11 15:43:53 +0300

    feat: standardize API_URL initialization across modules

--- app/api/public/index.ts
+++ app/api/public/index.ts
@@ -3,10 +3,10 @@ import { PublicProposal, PublicProposalResponse } from "@/types/new/public";
 import { TopicPriority, TopicPriorityResponse } from "@/types/new/topic-prioritization";
 
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
-export const API_URL = `${process.env.API_URL || API_BASE_URL}`;
+// export const API_URL = `${process.env.API_URL || API_BASE_URL}`;
 
 
-// export const API_URL = "/api";
+export const API_URL = "/api";
 
 export async function getPublicProposals(): Promise<PublicProposal[]> {
   const res = await fetch(`${API_URL}/v3/proposals/`);
